061c0c3ee9d6273aefc70da63020554e6ec4f49a
ryan-doucette <doucette.ry@northeastern.edu>
2022-11-25 13:50:03 -0500

    wired up mongodb cloud cluster env (for testing)

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
 "use strict";
+require('dotenv').config();
 
-// creating a base name for the mongodb
-const mongooseBaseName = "cs4550-api";
+// // creating a base name for the mongodb
+// const mongooseBaseName = "cs4550-api";
 
-// create the mongodb uri for development and test
-const database = {
-  development: `mongodb://localhost/${mongooseBaseName}-development`,
-  test: `mongodb://localhost/${mongooseBaseName}-test`,
-};
+// // create the mongodb uri for development and test
+// const database = {
+//   development: `mongodb://localhost/${mongooseBaseName}-development`,
+//   test: `mongodb://localhost/${mongooseBaseName}-test`,
+// };
 
-// Identify if development environment is test or development
-// select DB based on whether a test file was executed before `server.js`
-const localDb = process.env.TESTENV ? database.test : database.development;
+// // Identify if development environment is test or development
+// // select DB based on whether a test file was executed before `server.js`
+// const localDb = process.env.TESTENV ? database.test : database.development;
 
 // Environment variable MONGODB_URI will be available in
 // heroku production evironment otherwise use test or development db
-const currentDb = process.env.MONGODB_URI || localDb;
+const currentDb = process.env.MONGODB_URI || `mongodb://localhost/cs4550-api-development`;
 
 module.exports = currentDb;
