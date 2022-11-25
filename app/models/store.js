d223ae2073c8e83bab62e96197b362dc2b991416
britneyart80 <britneychen@britneys-mbp.mynetworksettings.com>
2022-11-25 14:08:52 -0500

    add product and store schema

---
+++ app/models/store.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const storeSchema = new mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Store", storeSchema);
